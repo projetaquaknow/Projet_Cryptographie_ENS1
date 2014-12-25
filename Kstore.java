d155ad91ae86a0e369e050d2a9b5ee87969abb2b
projetaquaknow <projetaquaknow@gmail.com>
2014-12-25 19:11:24 +0100

    Update Kstore.java
    - Ajout de la méthode de sauvegarde du Keystore.
    - Modification du constructeur du Keystore. Maintenant, il prend en entrée le type du Keystore et le mot de passe du Keystore.
    - Ce code travaille sur un Keystore appelé kstore.ks
    - Il faut implémenter l'insertion des certificats dans le Keystore.

--- Kstore.java
+++ Kstore.java
@@ -1,52 +1,118 @@
 package keystore;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.Certificate;
-import java.security.Key;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.DSAParams;
+import java.security.interfaces.DSAPrivateKey;
+import java.security.interfaces.DSAPublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.security.auth.x500.X500Principal;
+import java.security.Key;
 
-public class Kstore {
-	private KeyStore kstore;
-	private char[] kstorepwd;
+
+/**
+ * Classe permettant la manipulation
+ * aisée des Keystore.
+ * @author Maithili Vinayagamoorthi
+ * @author Cathie Prigent
+ * @author David Carmona-Moreno
+ */
+public class Kstore{
 	
-	//Constructeur du Keystore, pas de mdp généré pour le moment
-	private Kstore(String algoType) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
-		kstore = KeyStore.getInstance(algoType);
-		InputStream is = new BufferedInputStream(new FileInputStream(new File("kstore.ks")));
-		
-		kstore.load(is,kstorepwd);
-	}
-	
-	//Renvoie le certificat (clé publique encapsulée) associée à l'alias
-	private java.security.cert.Certificate getCertificate(String alias) throws KeyStoreException {
-		return kstore.getCertificate(alias);
-	}
-	
-	//Renvoie la clé privée associée à l'alias si le mdp est le bon
-	private Key getKey(String alias, char[] keypwd) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
-		return kstore.getKey(alias, keypwd);	//String à défaut d'autre chose pour l'instant
-	}
-	
-	//Permet de vérifier si l'alias a déjà une clé dans le Keystore
-	private boolean isKeyEntry(String alias) throws KeyStoreException {
-		return kstore.isKeyEntry(alias);
-	}
-	
-	//Remplissage du Keystore avec des valeurs bidons
-	//(aucune idée de quoi mettre dans Key et Chain, Key ne viendra qu'après le DN de toute manière, non ?)
-	private void populate() {
-		kstore.setKeyEntry("David", key, "david", chain);
-		kstore.setKeyEntry("Clément", key, "clement", chain);
-		kstore.setKeyEntry("Maithili", key, "maithili", chain);
-		kstore.setKeyEntry("Cathie", key, "cathie", chain);
-	}
-	
+	 // Le keystore de l'instance	
+     private KeyStore kstore;
+   
+     // Mot de passe du Keystore
+     private char[] kstorepwd;
+   
+     /**
+      * Constructeur de la classe Kstore
+      * @param algoType    Le type du Keystore
+      * @param passwd      Le mot de passe du Keystore
+      */
+     private Kstore(String algoType,char[] passwd) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+    	  
+    	  // Construction d'une instance d'un keystore de type type
+          kstore = KeyStore.getInstance(algoType);
+          
+          // Initialisation du keystore avec le contenu du fichier file
+          InputStream is = new BufferedInputStream(new FileInputStream(new File("kstore.ks")));
+          kstore.load(is,passwd);
+          
+          // Il faut garder le mot de passe du keystore pour l'utiliser par défaut
+          // lorsque l'utilisateur de la classe ne précise pas de mot de passe
+          // pour insérer une nouvelle entrée dans le keystore de l'instance
+          // (la seule méthode concernée est importSecretKey)
+          kstorepwd = passwd;
+     }
+     
+     /**
+      * Sauvegarde l'état courant du keystore manipulé dans le fichier file en le
+      * protégeant avec le mot de passe passwd.
+      * @param file Le fichier dans lequel sauvegarder le keystore de l'instance.
+      * @param passwd Le mot de passe protégeant le fichier créé.
+      */
+     public void save(File file, char[] passwd)
+             throws GeneralSecurityException, IOException {
+    	 
+         // Sérialise le contenu du keystore dans le flot attaché au fichier file
+         try (OutputStream os = new BufferedOutputStream(new FileOutputStream(file))) {
+             kstore.store(os, passwd);
+         }
+     }
+     
+     /**
+      * Sauvegarde l'état courant du keystore manipulé dans le fichier file en le
+      * protégeant avec le mot de passe passwd.
+      * @param alias L'alias de l'entrée du Keystore.
+      * @param keypwd Le mot de passe protégeant de l'entrée.
+      * @return key  Renvoi la clé privée associée à l'alias.
+      */
+     private Key getKey(String alias, char[] keypwd) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+    	 
+             return kstore.getKey(alias, keypwd); 
+     }
+     
+     /**
+      * Démonstration de la classe.
+      * @param args
+      */
+     public static void main(String[] args) throws GeneralSecurityException{
+    	 
+    	  try {
+			Kstore mystore=new Kstore("JCEKS","azerty".toCharArray());
+			 
+		} catch (KeyStoreException | NoSuchAlgorithmException
+				| CertificateException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    		
+     }
 }
+
