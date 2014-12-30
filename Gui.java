d4d096ab946550a3cbec2d81e899d0abedb4c658
projetaquaknow <projetaquaknow@gmail.com>
2014-12-30 12:13:21 +0100

    Update Gui.java
    - Si on clique sur le bouton "signer" et "vérifier" les fenetres "Siganture" et "Vérification" apparaissent.

--- Gui.java
+++ Gui.java
@@ -1,8 +1,11 @@
 /*
  * Gui.java
  * 
- * Verson mise à jour le 21 Décembre 2014
+ * Verson mise � jour le 21 D�cembre 2014
  * 
+ * @author Maithili Vinayagamoorthi
+ * @author Clément Chambat
+ * @author Cathie Prigent
  * @author David Carmona-Moreno
  * @version 1.1
  */
@@ -15,21 +18,18 @@ import javax.swing.SwingUtilities;
  * Gui
  * 
  * Dans la classe Gui on intancie tous 
- * les objets nécessaires à l'affichage
+ * les objets n�cessaires � l'affichage
  * d'une interface graphique. 
  * Une fenetre, un explorateur de fichiers,
- * les boutons "Signature" et "Vérification" 
- * et une autre fenetre affichant le DN et
- * le nom du fichier
+ * les boutons "Signature" et "V�rification", 
+ * une fenetre affichant le DN et
+ * le nom du fichier et une autre fenetre 
+ * pour la signature du fichier.
  */
 public class Gui {
 
 	public static void main(String[] args) {
 		
-		Verification verif = new Verification();
-		SignatureGUI mygui=new SignatureGUI();
-		
-		mygui.getSignatureGUI();
 		SwingUtilities.invokeLater(new Runnable() {
 	         @Override
 	         public void run() {
@@ -40,3 +40,4 @@ public class Gui {
 	  }
 
 }
+
