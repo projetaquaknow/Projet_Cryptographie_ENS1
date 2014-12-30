2a776df26ca9839842433b1388b50bbf89ad9fab
PrigentC <prigent.cathie@gmail.com>
2014-12-30 10:11:30 +0100

    Méthode de vérification du mot de passe
    Ajout nécessaire pour la vérification

--- Kstore.java
+++ Kstore.java
@@ -57,6 +57,15 @@ public class Kstore{
      }
      
      /**
+      * Vérifie si le mot de passe soumis au keystore est le bon
+      * @param pwd Le mot de passe que l'on soumet au keystore
+      * @return True si les mots de passe correspondent, False sinon
+      */
+     public boolean verifyPsw(char [] pwd) {
+         return kstorepwd==pwd;
+     }
+     
+     /**
       * Sauvegarde l'�tat courant du keystore manipul� dans le fichier file en le
       * prot�geant avec le mot de passe passwd.
       * @param file Le fichier dans lequel sauvegarder le keystore de l'instance.
@@ -79,7 +88,7 @@ public class Kstore{
       * @param alias L'alias de l'entr�e du Keystore.
       * @param keypwd Le mot de passe pour r�cup�rer la cl�
       */
-     private Key getKey(String alias, char[] keypwd) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+     public Key getKey(String alias, char[] keypwd) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
     	 
              return kstore.getKey(alias, keypwd); 
      }
