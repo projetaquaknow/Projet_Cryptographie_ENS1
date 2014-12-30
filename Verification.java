1ce5ef32eb9f758c4333dfd839cf89beb52f1b28
PrigentC <prigent.cathie@gmail.com>
2014-12-30 10:08:54 +0100

    Premières méthodes
    Implémentation des premières méthodes utiles à la vérification

---
+++ Verification.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crypto.verification;
+
+import java.security.Key;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import keyStore.Kstore;
+
+/**
+ *
+ * @author PrigentC
+ */
+public class Verification {
+    Kstore mystore;
+    
+    /**
+     * Vérifie si le mot de passe entré est le bon.
+     * @param pwd Le mot de passe saisi
+     * @return Vrai si le mot de passe est le bon, faux sinon
+     */
+    public boolean isPwdCorrect(char[] pwd) {
+        return mystore.verifyPsw(pwd);
+    }
+    
+    /**
+     * Fais récupérer la clé privée au keystore
+     * @param alias Le nom sous lequel est stocké la clé privée
+     * @param keypwd Le mot de passe protégeant la clé privée
+     * @return La clé privée recherchée
+     */
+    public Key retrievePrivKey(String alias, char[] keypwd) {
+        try {
+            return mystore.getKey(alias, keypwd);
+        } catch (UnrecoverableKeyException ex) {
+            Logger.getLogger(Verification.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (KeyStoreException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(Verification.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /*
+    Travail restant :
+    -Récupération de l'alias du keystore à partir de la signature
+    -Récupération des certificats (ou clé publique et DN directement) à partir du keystore
+    -Déchiffrage et affichage du DN
+    */
+    
+    
+}
