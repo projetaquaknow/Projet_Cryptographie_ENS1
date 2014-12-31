db8cf2833cfbfbd22f23417382204426efe06a45
projetaquaknow <projetaquaknow@gmail.com>
2014-12-31 12:55:40 +0100

    Update SignatureGUI.java
    - Ajout d'une méthode qui permet d'afficher le chemin du fichier choisi par l'utilisateur

--- SignatureGUI.java
+++ SignatureGUI.java
@@ -1,18 +1,23 @@
 /*
  * SignatureGUI.java
  *
- * Version mise à jour le 22 Décembre 2014
+ * Version mise � jour le 22 D�cembre 2014
  * 
  * @author Cathie Prigent
  * @version 1.1
  */
 
-
 package GUI;
 
+import Test_Cryptographie.CA;
 import java.awt.Container;
 import java.awt.GridLayout;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,42 +29,64 @@ import javax.swing.JTextField;
  * La classe SignatureGUI permet d'afficher
  * une fenetre qui montre le nom du fichier,
  * d'entrer le nom du signataire, le
- * prénom du signataire, le mot
+ * pr�nom du signataire, le mot
  * de passe secondaire et un bouton "Signer".
  * 
  */
 public class SignatureGUI {
 	
 	/**
-	  * Affiche la fenetre décrite 
-	  * précédemment
+	  * Affiche la fenetre d�crite 
+	  * pr�c�demment
 	  * 
 	  * @author         Cathie Prigent
 	  */
-	public static void getSignatureGUI() {
-		JFrame frame = new JFrame("Signature");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		Container contentPane = frame.getContentPane();
-	    contentPane.setLayout(new GridLayout(4,2));
-	    
-	    contentPane.add(new JLabel("File name : "));
-	    contentPane.add(new JLabel("Appropriate_FileName"));
-	    
-	    contentPane.add(new JLabel("Nom du signataire : "));
-	    contentPane.add(new JTextField(25));
-	    
-	    contentPane.add(new JLabel("Prénom du signataire : "));
-	    contentPane.add(new JTextField(25));
-	    
-	    contentPane.add(new JLabel("Mot de passe : "));
-	    contentPane.add(new JTextField(25));
-	    
-	    contentPane.add(new JButton("Signer"));
-	    
-	    frame.pack();
-	    frame.setVisible(true);
-	}
-	
+     public void getSignatureGUI() throws IOException {
+    	 
+         JFrame frame = new JFrame("Signature");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+         Container contentPane = frame.getContentPane();
+         
+         contentPane.setLayout(new GridLayout(4,2));
+         
+         // Permet de récupérer le chemin du fichier choisi par l'utilisateur
+         String m=this.Read_SignatureGUI("path.txt");
+         
+         contentPane.add(new JLabel("File name : "));
+         contentPane.add(new JLabel(m));
+         contentPane.add(new JLabel("Nom du signataire : "));
+         contentPane.add(new JLabel("Nom"));
+         contentPane.add(new JLabel("Prénom du signataire : "));
+         contentPane.add(new JLabel("Prénom"));
+         contentPane.add(new JLabel("Mot de passe : "));
+         contentPane.add(new JTextField(25));
+         contentPane.add(new JButton("Signer"));
+         
+         frame.pack();
+         frame.setVisible(true);
+     
+     }
+     
+    /**
+     * Méthode permettant de lire le contenu du fichier contenant le chemin du fichier choisi par l'utilisateur
+     * @param file Le nom du fichier contenant le chemin du fichier choisi par l'utilisateur
+     * @return  Le chemin du fichier
+     */
+    public String Read_SignatureGUI(String file) throws FileNotFoundException, IOException{
+        // Flux d'entrée ds données contenues dans un fichier
+             FileInputStream fileinput=new FileInputStream(new File("path.txt"));
+             
+             // Buffer de lecture
+             BufferedReader in=new BufferedReader(new FileReader("path.txt"));
+             
+             // Lecture de toute une ligne
+             String readline=in.readLine();
+             
+             in.close();
+             
+             return readline;
+    }
+
 }
 
