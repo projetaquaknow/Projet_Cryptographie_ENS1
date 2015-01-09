51c46073295c3da377d3e7ba0d6b10e9849f7af5
Maithili <maithili.vinayagamoorthi@uha.fr>
2015-01-09 14:43:17 +0100

    ButtonPanel Uptaded

--- ButtonPanel.java
+++ ButtonPanel.java
@@ -1,10 +1,11 @@
-/*
+/**
  * ButtonPanel.java
  * 
- * Verson mise � jour le 21 D�cembre 2014
+ * Verson mise a jour le 9 Janvier 2015
  * 
  * @author Maithili Vinayagamoorthi
- * @version 1.1
+ * @author David Carmona-Moreno
+ * @version 1.0
  */
 
 package GUI;
@@ -12,7 +13,9 @@ package GUI;
 import java.awt.event.*;
 import java.io.IOException;
 import javax.swing.*;
-
+import Verification.ProcVerificationTest;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * ButtonPanel
@@ -23,69 +26,71 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 class ButtonPanel extends JPanel implements ActionListener{
 	
-	private final JButton BoutonSignature;
-	private final JButton BoutonVerification;
+    private final JButton BoutonSignature;
+    private final JButton BoutonVerification;
 	
-	/**
-	 * Constructeur de la classe ButtonPanel
-	 */
-	public ButtonPanel() 
-	{
-	    BoutonSignature = new JButton("Sign");
-	    BoutonVerification = new JButton("Verify");
+    /**
+     * Constructeur de la classe ButtonPanel
+     * @author   Maithili Vinayagamoorthi
+     */
+    public ButtonPanel() 
+    {
+        BoutonSignature = new JButton("Sign");
+        BoutonVerification = new JButton("Verify");
 	    
-	    // Insertion des 2 boutons dans l'objet ButtonPanel
-	    add(BoutonSignature);
-	    add(BoutonVerification);
+        // Insertion des 2 boutons dans l'objet ButtonPanel
+        add(BoutonSignature);
+        add(BoutonVerification);
 	    
-	    // Les sources d'�v�nements sont d�clar�es � l'�couteur
-	    BoutonSignature.addActionListener(this);
-	    BoutonVerification.addActionListener(this);
-	}
+        // Les sources d'evenements sont declarees a l'ecouteur
+        BoutonSignature.addActionListener(this);
+        BoutonVerification.addActionListener(this);
+    }
 	
-	/**
-	 * Si on clique sur le bouton "Signature",
-	 * et qu'un fichier est choisi alors la fenêtre 
-         * de signature s'affiche.  
-	 * Si on clique sur le bouton "Vérification",
-         * et qu'un fichier est choisi alors la fenêtre 
-         * de vérification s'affiche.
-         * Si un fichier n'est pas choisi alors
-         * une fenêtre d'erreur s'ouvre
-	 * 
-	 * @param evt     Lecture de l'�v�nement souris
-	 * @author        Maithili Vinayagamoorthi
-	 * @version       1.1
-	 * 
-	 */
-        @Override
-	public void actionPerformed(ActionEvent evt) 	
-	{
-	    Object source = evt.getSource();
+    /**
+    * Si on clique sur le bouton "Signature",
+    * et qu'un fichier est choisi alors la fenetre 
+    * de signature s'affiche.  
+    * Si on clique sur le bouton "Verification",
+    * et qu'un fichier est choisi alors la fenetre 
+    * de verification s'affiche.
+    * Si un fichier n'est pas choisi alors
+    * une fenetre d'erreur s'ouvre
+    * 
+    * @param    evt Lecture de l'evenement souris
+    * @author   Maithili Vinayagamoorthi
+    * @author   David Carmona-Moreno
+    * 
+    */
+    @Override
+    public void actionPerformed(ActionEvent evt) 	
+    {
+	Object source = evt.getSource();
             
-            if(source==BoutonSignature){
-               SignatureGUI signaturegui=new SignatureGUI();
+        if(source==BoutonSignature){
+        SignatureGUI signaturegui=new SignatureGUI();
+        try {
+            signaturegui.getSignatureGUI();
+        } catch (IOException ex) {                    
+            final JPanel panel = new JPanel();
+            JOptionPane.showMessageDialog(panel, "Choose A File!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        }
+            
+        if(source==BoutonVerification){
+               
+            try {
                 try {
-                    signaturegui.getSignatureGUI();
-                } catch (IOException ex) {
-                    /**
-                     * A modifier s'il n'y a pas de fichier à signer
-                     */
-                    //Logger.getLogger(ButtonPanel.class.getName()).log(Level.SEVERE, null, ex);
-                    final JPanel panel = new JPanel();
-                    JOptionPane.showMessageDialog(panel, "Choose A File!", "Error", JOptionPane.ERROR_MESSAGE);
+                    ProcVerificationTest verif=new ProcVerificationTest();
+                } catch (NoSuchAlgorithmException | IOException ex) {
+                final JPanel panel = new JPanel();
+                JOptionPane.showMessageDialog(panel, "Choose a File!", "Error", JOptionPane.ERROR_MESSAGE);
                 }
+            } catch (GeneralSecurityException ex) {
+                final JPanel panel = new JPanel();
+                JOptionPane.showMessageDialog(panel, "Choose a File!", "Error", JOptionPane.ERROR_MESSAGE);
             }
-            
-            if(source==BoutonVerification){
-               Verification verif = new Verification();
-			   try {
-					verif.Verification();
-					
-			   } catch (IOException ex) {
-					final JPanel panel = new JPanel();
-                    JOptionPane.showMessageDialog(panel, "Choose A File!", "Error", JOptionPane.ERROR_MESSAGE);
-			   }
-            }
-	}
+                
+        }
+    }
 }
